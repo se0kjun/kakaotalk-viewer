b98768b5372b4caf65a43173911d0392ba8cf453
SeokJun Hong <hong921122@gmail.com>
2016-01-06 22:19:10 +0900

    update layout for react

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,33 @@
 
 var remote = require('remote');
 var dialog = remote.require('dialog');
+var parser_type = require('./src/scripts/parser');
+var mac_parser = require('./src/scripts/message_parser/mac_parser');
 
 var Event = {
 	open_file: function() {
-		dialog.showOpenDialog({properties: ['openFile']}, function(filename) {
-			document.getElementById('file_text').value = filename.toString();
-			addHistory(filename.toString()); 
-		});
+		dialog.showOpenDialog(
+			{
+				properties: ['openFile']
+			}, 
+			function(filename) {
+				document.getElementById('file_text').value = filename.toString();
+				addHistory(filename.toString()); 
+			});
 	}
 }
 
 function addHistory(filepath) {
+	a = new parser_type(filepath);
+	a.isMac();
+	b = new mac_parser(filepath);
+	b.message_parse(function(t, u, m) {
+		var node = document.createElement("div");
+		node.innerHTML = t;
+		document.getElementById('message_container').appendChild(node);
+		node.innerHTML = u;
+		document.getElementById('message_container').appendChild(node);
+		node.innerHTML = m;
+		document.getElementById('message_container').appendChild(node);
+	});
 }
