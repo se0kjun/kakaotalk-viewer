7fd14cb2812beeb043a58bd242012851e0768ad8
SeokJun Hong <hong921122@gmail.com>
2016-02-04 23:07:27 +0900

    add bower

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require('path');
+var webpack = require('webpack');
 
 var sourceRoot = path.join(path.join(__dirname, ''), 'src/scripts');
 
-module.exports = {
+var config = {
 	entry: path.join(sourceRoot, "renderer"),
 	target: "electron",
 	output: {
@@ -13,16 +14,40 @@ module.exports = {
 		loaders: [
 			{
 				test: /\.js$/,
-				exclude: /node_modules/,
+				exclude: /(node_modules|bower_components)/,
 				loader: 'babel-loader',
 				query: {
 					presets: ['react', 'es2015']
 				}
-			}
+			},
+			{
+			    test: /\.scss$/,
+			    loaders: 'style!css!sass'
+			},
+			{
+			    test: /\.css$/,
+			    loaders: ["style-loader", "css-loader"]
+			},
+			{
+			  	test: /\.less$/,
+			  	loader: "style!css!less"
+			},
+			{
+			    test: /\.json$/,
+			    loader: "json-loader" 
+			},
+
+			{ test: /\.woff$/,   loader: 'url?limit=5000&mimetype=application/font-woff' },
+			{ test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=8192" },
+			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" },
+			{ test: /\.png$/,    loader: 'url?limit=8192&mimetype=image/png' },
 		]
 	},
-	resolveLoader: {
-		extensions : ['', '.js', '.jsx'],
+	resolve: {
 		modulesDirectories: ['./node_modules']
 	}
 }
+
+module.exports = config;
