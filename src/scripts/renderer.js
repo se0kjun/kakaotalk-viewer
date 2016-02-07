217554f91795e47ffa34989233d5fc3dc84f3ff1
SeokJun Hong <hong921122@gmail.com>
2016-02-07 20:28:38 +0900

    add semantic

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,4 +1,5 @@
 
+require("semantic");
 var remote = window.require('remote');
 var dialog = remote.require('dialog');
 var parser_type = require('./parser');
@@ -42,6 +43,11 @@ var HistoryWrapper = React.createClass({
 });
 
 var MessageWrapper = React.createClass({
+	propTypes: {
+		histories: React.PropTypes.array.isRequired,
+		history_view: React.PropTypes.func.isRequired
+	},
+
 	getInitialState: function() {
 		return {
 			messages: [],
@@ -54,11 +60,15 @@ var MessageWrapper = React.createClass({
 			loading: true
 		});
 		var self = this;
-		var a = new parser_type(props.message_file);
-		a.isMac();
-		var b = new mac_parser(props.message_file);
 		var newArray = [];
-		var c = new mobile_parser(props.message_file);
+		var c;
+		if(props.message_type == "windows")
+			c = new window_parser(props.message_file);
+		else if(props.message_type == "mac")
+			c = new mac_parser(props.message_file);
+		else if(props.message_type == "mobile")
+			c = new mobile_parser(props.message_file);
+
 		c.message_parse(function(t, u, m, type) {
 			newArray.push(
 				{
@@ -75,25 +85,10 @@ var MessageWrapper = React.createClass({
 
 			return false;
 		});
-		// b.message_parse(function(t, u, m, type) {
-		// 	newArray.push(
-		// 		{
-		// 			time:t,
-		// 			user:u,
-		// 			message:m
-		// 		});
-		// }, function() {
-		// 	self.setState({
-		// 		loading: false,
-		// 		messages: newArray
-		// 	});
-
-		// 	return false;
-		// });
 	},
 
 	render: function() {
-		var class_name = "ui divided items loader";
+		var class_name = "ui divided items spin-loader";
 		if(!this.state.loading) {
 			class_name = "ui divided items unload";
 		}
@@ -153,6 +148,36 @@ var MenuNav = React.createClass({
 	}
 });
 
+var SelectMessageType = React.createClass({
+	render: function() {
+		return (
+			<div className="ui form">
+				<div className="inline fields">
+					<label>Where export this file</label>
+					<div className="field">
+						<div className="ui radio checkbox">
+							<input id="windows" className="hidden" name="message" type="radio" />
+							<label>Windows</label>
+						</div>
+					</div>
+					<div className="field">
+						<div className="ui radio checkbox">
+							<input id="mac" className="hidden" name="message" type="radio" />
+							<label>Mac</label>
+						</div>
+					</div>
+					<div className="field">
+						<div className="ui radio checkbox">
+							<input id="mobile" className="hidden" name="message" type="radio" />
+							<label>Mobile</label>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+});
+
 var App = React.createClass({
 	getInitialState: function() {
 		return {
@@ -169,8 +194,8 @@ var App = React.createClass({
 				properties: ['openFile']
 			},
 			function(filename) {
+				self.file_name = filename.toString();
 				self.setState({
-					file_name: filename.toString(),
 					histories: self.state.histories.concat([filename.toString()])
 				});
 			});
@@ -182,14 +207,26 @@ var App = React.createClass({
 		});
 	},
 
+	componentDidMount: function() {
+		var self = this;
+		$('.ui.dropdown').dropdown();
+		$('.ui.checkbox').checkbox({
+			onChange: function() {
+				self.setState({
+					message_type: $(this).closest('.checkbox').find('input').attr('id'),
+					file_name: self.file_name
+				});
+			}
+		});
+	},
+
 	render: function() {
 		return (
 			<div className="row">
 				<MenuNav histories={this.state.histories} history_view={this.openHistory} open_file={this.openFile} />
-				<div className="col-md-9">
-					<div id="message_container" className="message-wrapper">
-						<MessageWrapper message_file={this.state.file_name} />
-					</div>
+				<SelectMessageType select_messagetype={this.selectMessage} />
+				<div id="message_container" className="message-wrapper">
+					<MessageWrapper message_type={this.state.message_type} message_file={this.state.file_name} />
 				</div>
 			</div>
 		);
@@ -197,6 +234,6 @@ var App = React.createClass({
 });
 
 ReactDOM.render(
-	<App/>,
+	<App />,
 	document.getElementById("content-wrapper")
 );
